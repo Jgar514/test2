9d42eac1d0059e170c0019419c483d500bf86e2a
Joshua Garvey <j.garvey514@yahoo.com>
2024-02-27 23:10:40 -0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <div className='text-4xl bg-white justify-center flex flex-row'>Hello WOrld</div>
+  )
+}
+
+export default App
